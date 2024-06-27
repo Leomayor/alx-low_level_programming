57ef7f353b3b6bfebb7d3b85890da6821f0f5d4c
Leomayor <akinyemimichael34@gmail.com>
2024-06-27 14:47:32 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-int i;
+/*int i;*/
 
 printf("Infinite loop incoming :(\n");
 
@@ -15,9 +15,9 @@ i = 0;
 
 /*while (i < 10)*/
 
-/*{*/
+{
 /*putchar(i);*/
-/*}*/
+}
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
